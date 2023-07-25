a2bada63e8d9da0eb172dff602d7452134f61ad1
Denis <denis.ksens@gmail.com>
2023-07-25 14:47:12 +0300

    date format added

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import requests
 import unittest
 from pprint import pprint
 import xmltodict
+from datetime import datetime
 
 
 def cbr_cur_get(code, user_date):
-    pars = {'date_req': user_date}
+    date_object = datetime.strptime(user_date, "%Y-%m-%d").strftime("%d-%m-%Y")
+    pars = {'date_req': date_object}
     r = requests.get('https://www.cbr.ru/scripts/XML_daily.asp', params=pars)
     resp_dict = xmltodict.parse(r.text)
     for i in resp_dict['ValCurs']['Valute']:
         if i['CharCode'] == code:
-            return i['Value']
+            return f"{i['CharCode']} ({i['Name']}): {i['Value']}"
 
 
-resp = cbr_cur_get('USD', '02/03/2006')
+resp = cbr_cur_get('USD', '2007-03-02')
 pprint(resp)
